784664ef551b79d2ffa2046e9823c184495e22da
Jorge36 <jorgearobla@gmail.com>
2024-06-07 17:41:06 +0100

    v1

---
+++ src/com/company/Main.java
@@ -0,0 +1,144 @@
+package com.company;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static final String SOURCE_FILE = "./resources/many-flowers.jpg";
+    public static final String DESTINATION_FILE = "./out/many-flowers.jpg";
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+
+        BufferedImage originalImage = ImageIO.read(new File(SOURCE_FILE));
+        BufferedImage resultImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+	// We get time in Millis to compare one thread with more than one thread
+        long startTime = System.currentTimeMillis();
+
+        //recolorSingleThreaded(originalImage, resultImage);
+        int numberOfThreads = 8;
+
+        recolorMultithreaded(originalImage, resultImage, numberOfThreads);
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+
+        File outputFile = new File(DESTINATION_FILE);
+        ImageIO.write(resultImage, "jpg", outputFile);
+        System.out.println(String.valueOf(duration));
+    }
+
+    public static void recolorMultithreaded(BufferedImage originalImage, BufferedImage resultImage, int numberOfThreads) {
+        List<Thread> threads = new ArrayList<>();
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight() / numberOfThreads;
+	
+	// creates threads
+        for(int i = 0; i < numberOfThreads ; i++) {
+            final int threadMultiplier = i;
+
+            Thread thread = new Thread(() -> {
+                int xOrigin = 0 ;
+                int yOrigin = height * threadMultiplier;
+
+                recolorImage(originalImage, resultImage, xOrigin, yOrigin, width, height);
+            });
+
+            threads.add(thread);
+        }
+
+        for(Thread thread : threads) {
+            thread.start();
+        }
+
+        for(Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    // only one thread	
+    public static void recolorSingleThreaded(BufferedImage originalImage, BufferedImage resultImage) {
+        recolorImage(originalImage, resultImage, 0, 0, originalImage.getWidth(), originalImage.getHeight());
+    }
+
+    // recolor image, if it is done with several threads, every thread will work on a part of the image 
+    public static void recolorImage(BufferedImage originalImage, BufferedImage resultImage, int leftCorner, int topCorner,
+                                    int width, int height) {
+        for(int x = leftCorner ; x < leftCorner + width && x < originalImage.getWidth() ; x++) {
+            for(int y = topCorner ; y < topCorner + height && y < originalImage.getHeight() ; y++) {
+                recolorPixel(originalImage, resultImage, x , y);
+            }
+        }
+    }
+
+
+    public static void recolorPixel(BufferedImage originalImage, BufferedImage resultImage, int x, int y) {
+        int rgb = originalImage.getRGB(x, y);
+
+        int red = getRed(rgb);
+        int green = getGreen(rgb);
+        int blue = getBlue(rgb);
+
+        int newRed;
+        int newGreen;
+        int newBlue;
+
+	// if it is a gray flower, create new Red, Green and Blue
+        if(isShadeOfGray(red, green, blue)) {
+            newRed = Math.min(255, red + 10);
+            newGreen = Math.max(0, green - 80);
+            newBlue = Math.max(0, blue - 20);
+        } else { // or keep same color of the image
+            newRed = red;
+            newGreen = green;
+            newBlue = blue;
+        }
+        int newRGB = createRGBFromColors(newRed, newGreen, newBlue);
+        setRGB(resultImage, x, y, newRGB);
+    }
+
+    // Sets the RGB for a single pixel
+    public static void setRGB(BufferedImage image, int x, int y, int rgb) {
+        image.getRaster().setDataElements(x, y, image.getColorModel().getDataElements(rgb, null));
+    }
+
+    // check if it is a gray flower
+    public static boolean isShadeOfGray(int red, int green, int blue) {
+        return Math.abs(red - green) < 30 && Math.abs(red - blue) < 30 && Math.abs( green - blue) < 30;
+    }
+
+    // create new color to be assigned to the pixel in the gray flower
+    public static int createRGBFromColors(int red, int green, int blue) {
+        int rgb = 0;
+
+        rgb |= blue;
+        rgb |= green << 8;
+        rgb |= red << 16;
+
+        rgb |= 0xFF000000;
+
+        return rgb;
+    }
+
+    public static int getRed(int rgb) {
+        return (rgb & 0x00FF0000) >> 16;
+    }
+
+    public static int getGreen(int rgb) {
+        return (rgb & 0x0000FF00) >> 8;
+    }
+
+    public static int getBlue(int rgb) {
+        return rgb & 0x000000FF;
+    }
+
+
+}
